763898d44621185ecc89c33377a7c66e3002ab1e
Pablo Fernández Campos <pablofdezcvc1@hotmail.com>
2022-03-30 08:44:47 +0200

    Setting Routes

---
+++ app/controller/shirt.controller.js
@@ -0,0 +1,23 @@
+async function getInitializationData(req, res) {
+    console.log('Get Init Data');
+}
+
+async function getOneShirt(req, res) {
+    console.log('Get One Shirt');
+}
+
+async function getShirts(req, res) {
+    console.log('Get All Shirts');
+}
+
+async function newShirt(req, res) {
+    console.log('New shirt');
+}
+
+
+module.exports = {
+    getInitializationData,
+    getOneShirt,
+    getShirts,
+    newShirt
+}
--- app/routes/shirts.routes.js
+++ app/routes/shirts.routes.js
@@ -1,7 +1,11 @@
 const route = require('express').Router();
 
-route.get('/example', (req, res) => {
-    res.json({ message: 'This is a message' });
-});
+const { getInitializationData, getOneShirt, getShirts, newShirt } = require('../controller/shirt.controller');
+
+route.get('/getInitializationData', getInitializationData);
+route.get('./getOneShirt/:id', getOneShirt);
+route.get('./getShirts', getShirts);
+
+route.post('./newShirt', newShirt);
 
 module.exports = route;
